e5f0b7825f0d500b3d578f85f8c187da51a08323
cewarman <hank12451@gmail.com>
2026-07-24 20:01:50 +0800

    fixing

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 from setuptools import setup, Extension, find_packages
 
 
-module = Extension(
-    "rbtn.pypiRBTN",
-    sources=[
-        "C/RBTN.c",
-        "C/TNlib.c",
-    ],
-)
+ext_modules = [
+    Extension(
+        "rbtn.pypiRBTN",
+        sources=[
+            "C/RBTN.c",
+            "C/TNlib.c",
+        ],
+        extra_compile_args=[
+            "/std:c11",
+            "/O2",
+        ],
+    )
+]
 
 
 setup(
     name="rbtn",
     version="0.1.8",
-
     package_dir={"": "src"},
     packages=find_packages("src"),
-
-    ext_modules=[
-        module
-    ],
-
-    package_data={
-        "rbtn": [
-            "*.txt"
-        ]
-    },
+    ext_modules=ext_modules,
 )
