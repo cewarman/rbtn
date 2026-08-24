923c166c076ac4ac004edd7549e2c69ec8ccb786
cewarman <hank12451@gmail.com>
2026-08-25 07:05:23 +0800

    testing

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
+
 
 setup(
+    distclass=BinaryDistribution,
+
     package_dir={"": "src"},
     packages=find_packages("src"),
 
