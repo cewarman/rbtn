d0832bf11c908c862aa1442996ba2c2ff50039b1
cewarman <hank12451@gmail.com>
2026-08-25 09:56:31 +0800

    0.1.10

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from setuptools import setup, find_packages
 from setuptools.dist import Distribution
 
@@ -8,7 +6,6 @@ class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
-README = Path(__file__).parent / "src/rbtn/README.md"
 
 setup(
     distclass=BinaryDistribution,
@@ -26,7 +23,4 @@ setup(
     },
 
     include_package_data=True,
-
-    long_description=README.read_text(encoding="utf-8"),
-    long_description_content_type="text/markdown",
 )
